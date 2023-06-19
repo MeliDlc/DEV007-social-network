3225abfe41df5233c55040e10925ff09d38550dc
angy <angykikonitaphoto@gmail.com>
2023-06-19 11:04:18 -0600

    descomentar

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 // Este es el punto de entrada de tu aplicacion
 import { Home } from './Home.js';
 import { Register } from './Register.js';
-//import { Login } from './Login.js';
+import { Login } from './Login.js';
 
 const rootDiv = document.getElementById('root');
 
 const routes = {
   '/': Home,
   '/register': Register,
-  //'/login': Login,
+  '/login': Login,
 };
 
 export const onNavigate = (pathname) => {
