e9ac5b0066708a8061c3572095273075c75a8665
ohnoeslunablack <amidala_naboo@hotmail.com>
2023-07-04 11:40:38 -0600

    feat: juntar codigo

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,13 +1,23 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../firebase';
+//import { addDoc, collection } from "firebase/firestore";
+import { auth, db } from '../firebase';
 
 export const crearUsuarioConCorreoYContraseña = (email, contraseña) => {
   return createUserWithEmailAndPassword(auth, email, contraseña);
 
 };
 
-export const registrarUsuarioConCorreoYContraseña = (nombre ,email, contraseña) => {
+export const registrarUsuarioConCorreoYContraseña = (nombre, email, contraseña) => {
   return signInWithEmailAndPassword(auth, nombre, email, contraseña);
 
 };
+
+/*export const agregarUnNuevoPost = (contenido, db, auth) => {
+  return addDoc(collection(db,'posts'), {
+    contenido: contenido,
+    usuario: auth.currentUser.email,
+    datetime: new Date()
+  });
+};*/
+
