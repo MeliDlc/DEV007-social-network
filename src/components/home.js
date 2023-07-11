e3a9d6d3768d84cc13915af1e7e0a517b48883db
ohnoeslunablack <amidala_naboo@hotmail.com>
2023-07-11 11:37:25 -0600

    no jala cambios

--- src/components/home.js
+++ src/components/home.js
@@ -4,15 +4,22 @@ export const home = (onNavigate) => {
 
   homeDiv.innerHTML += `
   <div class="contenedorImg">
+   <div class="image-circle">
       <img src="img/viajando.png" class="logo">
+      </div>
+      </div>
   `;
+
+  const buttonContainer = document.createElement('div');
+  buttonContainer.className = 'wrap';
 
   const buttonRegister = document.createElement('button');
   buttonRegister.textContent = 'Inicia';
-  buttonRegister.className = 'botonInicio';
+  buttonRegister.className = 'buttonHome';
 
   buttonRegister.addEventListener('click', () => onNavigate('/login'));
   homeDiv.appendChild(buttonRegister);
+  homeDiv.appendChild(buttonContainer);
 
   return homeDiv;
 };
